407dab0842381601c0de1538219e8fb8e449de17
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-07-16 01:28:00 -0300

    Spicing the model

--- models/user.js
+++ models/user.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-	username: { type: String, required: true },
-	password: { type: String, required: true },
-	name: { type: String, required: true }
+	username: { type: String },
+	password: { type: String },
+	firstName: { type: String },
+	lastName: { type: String },
+	email: { type: String },
+	profileEnum: { type: String },
+	profilePhoto: { type: String }
 });
 
 module.exports = mongoose.model('User', userSchema);
