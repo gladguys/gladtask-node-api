fef8a062d793425764c62cb44eaa5640eee7254b
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-07-17 12:33:11 -0300

    Adding the post route

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -6,4 +6,6 @@ const ProjectController = require('../controllers/projectController');
 router.get('/', checkToken, ProjectController.get);
 router.get('/user/:userId', checkToken, ProjectController.getProjectsByUser);
 
+router.post('/', checkToken, ProjectController.post);
+
 module.exports = router;
