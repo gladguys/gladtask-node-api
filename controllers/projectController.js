1fe0267c788d8760ad6467bd9542bf837a5fe94f
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-07-17 12:32:52 -0300

    IMplement save of project

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,5 +1,6 @@
 const HttpStatus = require('http-status-codes');
 
+const jwtHelper = require('../jwtHelper');
 const Project = require('../models/project');
 
 exports.get = (req, res) => {
@@ -10,4 +11,12 @@ exports.getProjectsByUser = async (req, res) => {
 	const userId = req.params['userId'];
 	const userProjects = await Project.find({ participants: userId });
 	res.status(HttpStatus.OK).json(userProjects);
+};
+
+exports.post = async (req, res) => {
+	const creatorUserId = jwtHelper.getUserIdFromToken(req.headers['authorization']);
+	const project = new Project(req.body);
+	project.participants = [creatorUserId];
+	const projectSaved = await project.save();
+	res.status(HttpStatus.CREATED).send({ project: projectSaved });
 };
