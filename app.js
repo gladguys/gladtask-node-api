3f3cbdb02aa76758e524fc65add3c62463b09ed4
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-07-25 12:43:49 -0300

    Adding the route to the docs

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 //const DefaultErrorHandlerMiddleware = require('./middlewares/defaultErrorHandlerMiddleware');
 
@@ -32,6 +34,7 @@ app.use('/api/teams', teamRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/invitations', invitationRoutes);
 app.use('/api/emails', emailRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //app.use(DefaultErrorHandlerMiddleware);
 
