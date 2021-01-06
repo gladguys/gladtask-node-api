ea5398d2775256926b6ce91a88d4dcabed38d7f7
Denis <dineresc@gmail.com>
2021-01-05 23:12:37 -0300

    sobe codigo

--- models/timeSpent.js
+++ models/timeSpent.js
@@ -5,6 +5,8 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const timeSpentSchema = mongoose.Schema({
 	_id: { type: ObjectId, auto: true },
 	taskId: { type: ObjectId },
+	firstName: { type: String},
+	lastName: { type: String},
 	gladname: { type: String },
 	minutesSpent: { type: Number },
 	date: { type: Date }
