423486fa68575dbf34e3f1677ca32757d18df7f4
Denis <dineresc@gmail.com>
2021-01-28 13:20:52 -0300

    adding embbeded projects into teams collection

--- models/project.js
+++ models/project.js
@@ -1,20 +1,18 @@
 import * as mongoose from 'mongoose';
 
 import { User } from './user';
-import { Team } from './team';
 
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const projectSchema = mongoose.Schema({
+const ProjectSchema = mongoose.Schema({
 	_id: { type: ObjectId, auto: true },
 	name: { type: String },
 	description: { type: String },
 	manager: { type: ObjectId, ref: User, autopopulate: true },
-	team: { type: ObjectId, ref: Team, autopopulate: true },
 	creationDate: { type: Date },
 	participants: [{ type: ObjectId, ref: User, autopopulate: true }],
 	projectImage: { type: String }
 });
 projectSchema.plugin(require('mongoose-autopopulate'));
 
-export const Project = mongoose.model('Project', projectSchema);
+export default ProjectSchema;
--- models/team.js
+++ models/team.js
@@ -1,6 +1,7 @@
 import * as mongoose from 'mongoose';
 
 import { User } from './user';
+import { ProjectSchema } from './project';
 
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
@@ -8,6 +9,7 @@ const teamSchema = mongoose.Schema({
 	_id: { type: ObjectId, auto: true },
 	name: { type: String },
 	manager: { type: ObjectId, ref: User, autopopulate: true },
+	projects: [ProjectSchema],
 	participants: [{ type: ObjectId, ref: User, autopopulate: true }]
 });
 teamSchema.plugin(require('mongoose-autopopulate'));
